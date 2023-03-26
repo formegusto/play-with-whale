d17cfb2e247b251ac55da3d5f8de7162f1cb0ccf
formegusto <formegusto@gmail.com>
2023-03-26 21:22:45 +0900

    간단한 nodeJS앱 컨테이너로 만들어보기 (2)

--- docker-ci/#04/server.js
+++ docker-ci/#04/server.js
@@ -5,9 +5,18 @@ const PORT = 8080;
 const app = Express();
 
 app.get("/", (req, res) => {
-  return res.send(`<h1>Hello It's Docker with NodeJS 🐳</h1>`);
+  return res.send(`
+    <html>
+    <head>
+      <title>iamwhale.</title>
+    </head>
+    <body>
+      <h1>Hello It's Docker with NodeJS 🐳</h1>
+      <h2>안녕! 나는 고래에요! 🤚🏻</h2>
+    </body>
+    </html>`);
 });
 
-app.listen(PORT, HOST, () => {
+app.listen(PORT, () => {
   console.log(`[Express] Server(${HOST}:${PORT}) Start`);
 });
