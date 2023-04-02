4216409d6d3d426f40312e2937fdbc0cf151815e
formegusto <formegusto@gmail.com>
2023-04-02 18:54:35 +0900

    docker 예제 준비완

---
+++ docker-network/src/index.js
@@ -0,0 +1,31 @@
+import Express from "express";
+import morgan from "morgan";
+import axios, { HttpStatusCode } from "axios";
+import process from "process";
+
+const [_1, _2, myhost, myport] = process.argv;
+const app = Express();
+
+app.use(morgan("dev"));
+app.use(Express.json());
+
+app.get("/", (req, res) => {
+  return res.status(HttpStatusCode.Ok).json({
+    message: `Hello, I'm ${myhost}:${myport}`,
+  });
+});
+
+app.post("/", async (req, res) => {
+  const { host, port } = req.body;
+
+  const data = (await axios.get(`http://${host}:${port}`)).data;
+
+  return res.status(HttpStatusCode.Ok).json({
+    now: `Hello, I'm ${myhost}:${myport}`,
+    next: data,
+  });
+});
+
+app.listen(myport, () => {
+  console.log("[Express] Start Server :)");
+});
